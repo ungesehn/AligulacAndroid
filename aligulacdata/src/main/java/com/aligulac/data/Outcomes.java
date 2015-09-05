9ffda85f3de465c62c45af1b67b12789b2c9395b
Anton <mastaa@gmail.com>
2015-09-05 12:41:18 +0200

    Added outcomes table to result view, added convenience method to Outcomes (stats)

--- aligulacdata/src/main/java/com/aligulac/data/Outcomes.java
+++ aligulacdata/src/main/java/com/aligulac/data/Outcomes.java
@@ -2,31 +2,35 @@
 package com.aligulac.data;
 
 public class Outcomes {
-    private Number prob;
-    private Number sca;
-    private Number scb;
+  private Number prob;
+  private Number sca;
+  private Number scb;
 
-    public Number getProb() {
-        return this.prob;
-    }
+  public Number getProb() {
+    return this.prob;
+  }
 
-    public void setProb(Number prob) {
-        this.prob = prob;
-    }
+  public void setProb(Number prob) {
+    this.prob = prob;
+  }
 
-    public Number getSca() {
-        return this.sca;
-    }
+  public Number getSca() {
+    return this.sca;
+  }
 
-    public void setSca(Number sca) {
-        this.sca = sca;
-    }
+  public void setSca(Number sca) {
+    this.sca = sca;
+  }
 
-    public Number getScb() {
-        return this.scb;
-    }
+  public Number getScb() {
+    return this.scb;
+  }
 
-    public void setScb(Number scb) {
-        this.scb = scb;
-    }
+  public void setScb(Number scb) {
+    this.scb = scb;
+  }
+
+  public String getScoreCombined() {
+    return sca.toString() + "-" + scb.toString();
+  }
 }
--- app/src/main/java/com/aligulac/app/ResultActivity.java
+++ app/src/main/java/com/aligulac/app/ResultActivity.java
@@ -2,9 +2,13 @@ package com.aligulac.app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import com.aligulac.data.Outcomes;
 import com.aligulac.data.PredictMatch;
 
 import java.text.NumberFormat;
@@ -24,7 +28,7 @@ public class ResultActivity extends AppCompatActivity implements ResultHandler {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_result);
+    setContentView(R.layout.activity_result2);
     ButterKnife.bind(this);
 
     Bundle bundle = getIntent().getExtras();
@@ -48,6 +52,30 @@ public class ResultActivity extends AppCompatActivity implements ResultHandler {
       nf.setMaximumFractionDigits(2);
       mProbabilityPlayer1.setText(nf.format(prediction.getProba()));
       mProbabilityPlayer2.setText(nf.format(prediction.getProbb()));
+
+      //add outcomes to table
+      View root = findViewById(R.id.outcomes_table);
+      int rows = Math.round(prediction.getOutcomes().size() / 2);
+      for (int i = 0; i < rows; i++) {
+        Outcomes out1 = prediction.getOutcomes().get(i);
+        Outcomes out2 = prediction.getOutcomes().get(i + rows);
+
+        View row = LayoutInflater.from(this).inflate(R.layout.elem_outcome, (ViewGroup) root, false);
+        TextView tv;
+        //set outcomes
+        tv = ButterKnife.findById(row, R.id.outcome_p1);
+        tv.setText(nf.format(out1.getProb()));
+        tv = ButterKnife.findById(row, R.id.outcome_p2);
+        tv.setText(nf.format(out2.getProb()));
+        //set stats
+        tv = ButterKnife.findById(row, R.id.stats_p1);
+        tv.setText(out1.getScoreCombined());
+        tv = ButterKnife.findById(row, R.id.stats_p2);
+        tv.setText(out2.getScoreCombined());
+        //add to table
+        ((ViewGroup) root).addView(row);
+      }
+
     }
   }
 }
